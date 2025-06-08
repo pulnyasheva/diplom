dbf1fc63971ab39a302e1882a894856ab64bd950
pulnyasheva <pulaa72@gmail.com>
2025-06-08 17:28:45 +0300

    change dockerfile

--- generate.py
+++ generate.py
@@ -11,13 +11,24 @@ def random_array():
     return [random.randint(1, 100) for _ in range(random.randint(1, 10))]
 
 def connect_db():
-    return psycopg2.connect(
-        dbname='postgres',
-        user='postgres',
-        password='postgres',
-        host='postgres',
-        port='5432'
-    )
+    attempts = 5
+    for attempt in range(attempts):
+        try:
+            conn = psycopg2.connect(
+                dbname='postgres',
+                user='postgres',
+                password='postgres',
+                host='postgres',
+                port='5432'
+            )
+            print("Подключение успешно!")
+            return conn
+        except psycopg2.OperationalError as e:
+            print(f"Ошибка подключения: {e}. Попытка {attempt + 1} из {attempts}.")
+            time.sleep(5)
+
+    print("Не удалось подключиться к базе данных после 5 попыток.")
+    return None
 
 def populate_tables(conn):
     cursor = conn.cursor()
@@ -72,7 +83,8 @@ def populate_tables(conn):
 
 if __name__ == '__main__':
     conn = connect_db()
-    try:
-        populate_tables(conn)
-    finally:
-        conn.close()
+    if conn:
+        try:
+         populate_tables(conn)
+        finally:
+            conn.close()
--- wait-for-postgres.sh
+++ wait-for-postgres.sh
@@ -10,6 +10,7 @@ check_table_and_rows() {
   local table_name="$1"
   local min_rows="$2"
 
+  export PGPASSWORD="$POSTGRES_PASSWORD"
   row_count=$(psql -h "$host" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -t -c "SELECT COUNT(*) FROM "$table_name";")
 
   if [[ "$row_count" -ge "$min_rows" ]]; then
@@ -25,7 +26,7 @@ min_rows=4
 
 until check_table_and_rows "$table1" "$min_rows" && check_table_and_rows "$table2" "$min_rows"; do
   >&2 echo "Waiting for the database tables to be initialized with enough rows..."
-  sleep 1
+  sleep 2
 done
 
 >&2 echo "Postgres is up, both tables are ready and contain enough rows - executing command"
